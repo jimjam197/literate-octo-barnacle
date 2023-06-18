2a0213905798097bcc162bd811a709426bad72bc
jimjam197 <Sjc97@live.com>
2023-06-18 16:22:22 -0400

    ### Ticket: `#U3_02` status: complete

--- script.js
+++ script.js
@@ -63,3 +63,75 @@ export const domDisplay = (playerInput) => {
 
     // Your code here
 }
+
+class Room {
+    constructor(name, description, items, inventory = []) {//makes it so if there is nothing it can come up as nothing
+        this.name = name;
+        this.description = description;
+        this.items = items
+        this.inventory = inventory;
+    }
+}
+
+const roomLookup = {//some room discriptions
+    outside: "Its a pleasant day in the area and you are still late to your meeting.",
+    mudroom: "A small mud room with tiled floors, coat hooks, and a bench for taking off shoes.",
+    mainroom: "A cozy main room with a plush sofa, a wooden coffee table, and soft lighting.",
+    office: "A tidy office room with a large wooden desk, a comfortable chair, and a window overlooking the street."
+}
+
+const rooms = {//room set up essentually 
+    outside: new Room("Sidewalk", "Its a pleasent day in the area and you are still late to your meeting.", [], []
+    ),
+    mudroom: new Room(
+        "mudroom", "A small mud room with tiled floors, coat hooks, and a bench for taking off shoes.", [], []
+    ),
+    mainroom: new Room(
+        "mainroom", "A cozy main room with a plush sofa, a wooden coffee table, and soft lighting.", [], []
+    ),
+    office: new Room(
+        "office", "A tidy office room with a large wooden desk, a comfortable chair, and a window overlooking the street.", [], []
+    )
+}
+
+class Item {
+    constructor(name, disc, location) {
+        this.name = name;
+        this.disc = disc;
+        this.location = location;
+    }
+}
+
+const items = {
+    keycard: new Item("Key card", "A key card with a picture of you on it from your first day as well as your name on it", "outside"),
+    notepad: new Item("Notepad", "A note pad that seems to have some doodles on it of assorted animals", "office"),
+    book: new Item("Book", "A well read book that someone must have dropped", "mudroom")
+}
+
+let currentMovementState = 'outside';
+//Make a state machine where you can go from the sidewalk to any area
+
+const movement = {
+    office: ['mainroom'],
+    mainroom: ['mudroom', 'office'],
+    mudroom: ['outside', 'mainroom'],
+    outside: ['mudroom']
+}
+
+function start() {
+    console.log(gameDetails.startingRoomDescription)
+    while (true) {
+        console.log(`you are currently at ${currentMovementState}`)
+        console.log(`${roomLookup[currentMovementState]}`)
+        let location = prompt(`Where do you want to go? `)
+        let possibleStates = Object.keys(rooms)
+
+        if (possibleStates.includes(location)) {
+            currentMovementState = location;
+            console.log(currentMovementState);
+        } else {
+            console.log('Invalid input')
+        }
+    }
+}
+start();
